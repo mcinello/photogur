575906302d50eda28cdea8a8e521ee6c7e8f9bf9
Michelle Cinello <m.cinello@gmail.com>
2017-10-19 00:29:14 -0400

    Trying new by-year query

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -17,7 +17,8 @@ class Picture < ApplicationRecord
   end
 
   def self.pictures_created_in_year(year)
-    Picture.where("cast(strftime('%Y', created_at) as int) = ?", year)
+    Picture.where('created_at LIKE "%#{year}%"')
+    # Picture.where("cast(strftime('%Y', created_at) as int) = ?", year)
   end
 
 end
